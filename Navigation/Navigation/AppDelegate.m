32eb3cd4fe3522b1a99914282d6444cdfe9f0a5c
Brennan Stehling <brennan@smallsharptools.com>
2015-05-20 22:27:27 -0700

    changed to acme

--- Navigation/Navigation/AppDelegate.m
+++ Navigation/Navigation/AppDelegate.m
@@ -3,7 +3,7 @@
 //  Navigation
 //
 //  Created by Brennan Stehling on 5/20/15.
-//  Copyright (c) 2015 Striiv. All rights reserved.
+//  Copyright (c) 2015. All rights reserved.
 //
 
 #import "AppDelegate.h"
